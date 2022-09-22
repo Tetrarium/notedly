4f722b5532116e8225c2f2c7f3553d16dbc2030c
Alexandr Turov <alexandr985crv@gmail.com>
2022-09-23 01:48:30 +0700

    Chapter 6. CRUD operation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // This is the main entry point of our application
 
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
 
 const db = require('./db');
@@ -10,51 +10,17 @@ const db = require('./db');
 // Импортируем модели БД в приложение
 const models = require('./models');
 
+// Построение схемы с использованием языка схем GraphQL
+const typeDefs = require('./shema');
+
+// Представляем функцию разрешения для полей схемы
+const resolvers = require('./resolvers');
+
 // Запускаем сервер на порте, указанном в файле .env, или на порте 4000
 const port = process.env.PORT || 4000;
 
 // Сохраняем значение DB_HOST в виде переменной
 const DB_HOST = process.env.DB_HOST;
-
-
-// Построение схемы с использованием языка схем GraphQL
-const typeDefs = gql`
-	type Query {
-		hello: String
-		notes: [Note]
-		note(id: ID): Note
-	}
-	type Note {
-		id: ID
-		content: String
-		author: String
-	}
-	type Mutation {
-		newNote(content: String!): Note
-	}
-`;
-
-// Представляем функцию разрешения для полей схемы
-const resolvers = {
-	Query: {
-		hello: () => 'Hello world!',
-		notes: async () => {
-			return await models.Note.find();
-		},
-		note: async (parent, args) => {
-			return await models.Note.findById(args.id);
-		}
-	},
-	Mutation: {
-		newNote: async (parent, args) => {
-			
-			return await models.Note.create({
-				content: args.content,
-				author: "Alexandr Turov"
-			});
-		}
-	}
-};
 
 const app = express();
 
@@ -63,7 +29,13 @@ db.connect(DB_HOST);
 
 
 // Настройка ApolloServer
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: () => {
+		return { models };
+	}
+});
 
 // Применяем промежуточное ПО Apollo GraphQL и указываем путь к /api
 server.applyMiddleware({app, path: '/api'});
@@ -71,17 +43,3 @@ server.applyMiddleware({app, path: '/api'});
 app.listen({ port }, () => 
 	console.log(`GrathQL Server running at http://localhost:${port}${server.graphqlPath}`)
 );
-
-/*
-app.get('/', (req, res) => res.send('Hello Web Server!!!'));
-
-app.listen(port, () =>
-	console.log(`Server running at http://localchost:${port}`)
-);
-*/
-
-let notes = [
-	{id: '1', content: 'This is a note', author: 'Adam Scott'},
-	{id: '2', content: 'This is another note', author: 'Harlow Everly'},
-	{id: '3', content: 'Oh hey look, another note!', author: 'Alexandr Turov'}
-]
